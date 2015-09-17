edad1772ec1f1d61ce860aaacc3499b3983f435e
Malte Skoruppa <skoruppa@cs.uni-saarland.de>
2015-09-17 10:44:27 +0200

    improved a test

--- test-own/rare.php
+++ test-own/rare.php
@@ -1,6 +1,10 @@
 <?php
 
-/* Testing some rather rare constructions */
+/*
+ Testing some rather rare constructions (which are described in
+ https://github.com/nikic/php-ast/issues/12, but I have not seen in
+ pratice yet)
+*/
 
 // AST_UNPACK
 // https://wiki.php.net/rfc/argument_unpacking
@@ -25,3 +29,45 @@ class T {
 // AST_GROUP_USE
 // https://wiki.php.net/rfc/group_use_declarations
 use Foo\Bar\{ A, B };
+
+
+/*
+ Other way around: not described in
+ https://github.com/nikic/php-ast/issues/12, but seen in pratice.
+*/
+
+// AST_CLOSURE_USES
+function($foo) use ($bar, $buzz) {};
+
+class blah {
+  // AST_PROP_DECL
+  static $buzz, $hui;
+  const bla = 2, yay = 3;
+
+  // AST_TRAIT_ADAPTATIONS
+  use UserCreationTrait {
+    createUser as drupalCreateUser;
+    createRole as drupalCreateRole;
+    createAdminRole as drupalCreateAdminRole;
+  }
+
+}
+
+// AST_CONST_DECL
+const ha = 1, ho = 2;
+
+// AST_ARRAY
+[1,2];
+
+// AST_IF
+if(true){}
+elseif(false){}
+elseif(false&&true) {}
+elseif(true&&false) {}
+elseif(true||true) {}
+else {}
+
+// AST_USE
+use this, that;
+
+// ...and all *_LIST nodes which have a variable number of children
