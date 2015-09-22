75cc9d12e727ea673a2f53caf84c69c0d082080d
Malte Skoruppa <skoruppa@cs.uni-saarland.de>
2015-09-22 18:25:13 +0200

    improved vulnerable sql code test file

--- vulnerablephp/sql_queries.php
+++ vulnerablephp/sql_queries.php
@@ -22,7 +22,7 @@ function connect( $user, $pwd, $server, $database) {
 
 // perform a query with user-supplied input (VULNERABLE!)
 // using AST_ENCAPS_LIST
-function perform_query1( $firstname, $lastname) {
+function perform_query1( $link, $firstname, $lastname) {
   // formulate query
   $query = "SELECT firstname, lastname, age FROM friends 
     WHERE firstname='$firstname' AND lastname='$lastname'";
@@ -30,7 +30,7 @@ function perform_query1( $firstname, $lastname) {
   echo "Query 1:", PHP_EOL, $query, PHP_EOL;
 
   // perform query
-  $result = mysql_query($query);
+  $result = mysql_query($query, $link);
   
   // check result
   if( !$result) {
@@ -44,7 +44,7 @@ function perform_query1( $firstname, $lastname) {
 
 // perform a query with user-supplied input (VULNERABLE!)
 // using AST_BINARY_OP
-function perform_query2( $firstname, $lastname) {
+function perform_query2( $link, $firstname, $lastname) {
   // formulate query
   $query2 = "SELECT firstname, lastname, age FROM friends 
     WHERE firstname='".$firstname."' AND lastname='".$lastname."'";
@@ -52,7 +52,7 @@ function perform_query2( $firstname, $lastname) {
   echo "Query 2:", PHP_EOL, $query2, PHP_EOL;
 
   // perform query (let's use Postgres instead... this file won't run, but the AST doesn't mind)
-  $result = pg_query($query2);
+  $result = pg_query($link, $query2);
   
   // check result
   if( !$result) {
@@ -66,7 +66,7 @@ function perform_query2( $firstname, $lastname) {
 
 // perform a query with user-supplied input (VULNERABLE!)
 // using AST_ASSIGN_OP
-function perform_query3( $firstname, $lastname) {
+function perform_query3( $link, $firstname, $lastname) {
   // formulate query
   $query3 = "SELECT firstname, lastname, age FROM friends WHERE firstname='";
   $query3 .= $firstname;
@@ -77,7 +77,7 @@ function perform_query3( $firstname, $lastname) {
   echo "Query 3:", PHP_EOL, $query3, PHP_EOL;
 
   // perform query (let's use SQlite instead... this file won't run, but the AST doesn't mind)
-  $result = sqlite_query($query3);
+  $result = sqlite_query($link, $query3);
   
   // check result
   if( !$result) {
@@ -90,7 +90,7 @@ function perform_query3( $firstname, $lastname) {
 }
 
 // perform a FIXED query -- no user input (NOT vulnerable!)
-function perform_query4() {
+function perform_query4($link) {
   // formulate query
   $query4 = "SELECT firstname, lastname, age FROM friends 
     WHERE firstname='fred' AND lastname='fox'";
@@ -98,7 +98,7 @@ function perform_query4() {
   echo "Query 4:", PHP_EOL, $query4, PHP_EOL;
 
   // perform query
-  $result = mysql_query($query4);
+  $result = mysql_query($query4, $link);
   
   // check result
   if( !$result) {
@@ -112,13 +112,13 @@ function perform_query4() {
 
 // perform a query with user-supplied input (VULNERABLE!)
 // using AST_ENCAPS_LIST
-function perform_query5( $firstname, $lastname) {
+function perform_query5( $link, $firstname, $lastname) {
 
   echo "Query 5:", PHP_EOL;
 
   // perform query
-  $result = mysql_query("SELECT firstname, lastname, age FROM friends 
-    WHERE firstname='$firstname' AND lastname='$lastname'");
+  $result = sqlite_query("SELECT firstname, lastname, age FROM friends 
+    WHERE firstname='$firstname' AND lastname='$lastname'", $link);
   
   // check result
   if( !$result) {
@@ -131,12 +131,12 @@ function perform_query5( $firstname, $lastname) {
 
 // perform a query with user-supplied input (VULNERABLE!)
 // using AST_BINARY_OP
-function perform_query6( $firstname, $lastname) {
+function perform_query6( $link, $firstname, $lastname) {
 
   echo "Query 6:", PHP_EOL;
 
   // perform query
-  $result = mysql_query("SELECT firstname, lastname, age FROM friends 
+  $result = sqlite_query($link, "SELECT firstname, lastname, age FROM friends 
     WHERE firstname='".$firstname."' AND lastname='".$lastname."'");
   
   // check result
@@ -158,7 +158,7 @@ $link = connect( $user, $pwd, $server, $database);
 //$result = perform_query( "fred' UNION SELECT firstname, lastname, pwd from friends; #", "fox");
 
 // Using perform_query1()
-$result = perform_query1( "fred", "fox");
+$result = perform_query1( $link, "fred", "fox");
 echo "Result:", PHP_EOL;
 while( $row = mysql_fetch_assoc( $result)) {
   echo $row['firstname'], "\t|\t",
@@ -167,9 +167,8 @@ while( $row = mysql_fetch_assoc( $result)) {
 }
 echo PHP_EOL;
 
-/*
 // Using perform_query2()
-$result = perform_query2( "fred", "fox");
+$result = perform_query2( $link, "fred", "fox");
 echo "Result:", PHP_EOL;
 while( $row = mysql_fetch_assoc( $result)) {
   echo $row['firstname'], "\t|\t",
@@ -179,7 +178,7 @@ while( $row = mysql_fetch_assoc( $result)) {
 echo PHP_EOL;
 
 // Using perform_query3()
-$result = perform_query3( "fred", "fox");
+$result = perform_query3( $link, "fred", "fox");
 echo "Result:", PHP_EOL;
 while( $row = mysql_fetch_assoc( $result)) {
   echo $row['firstname'], "\t|\t",
@@ -187,10 +186,9 @@ while( $row = mysql_fetch_assoc( $result)) {
     $row['age'], PHP_EOL;
 }
 echo PHP_EOL;
-*/
 
 // Using perform_query4()
-$result = perform_query4();
+$result = perform_query4($link);
 echo "Result:", PHP_EOL;
 while( $row = mysql_fetch_assoc( $result)) {
   echo $row['firstname'], "\t|\t",
@@ -200,7 +198,7 @@ while( $row = mysql_fetch_assoc( $result)) {
 echo PHP_EOL;
 
 // Using perform_query5()
-$result = perform_query5( "fred", "fox");
+$result = perform_query5($link, "fred", "fox");
 echo "Result:", PHP_EOL;
 while( $row = mysql_fetch_assoc( $result)) {
   echo $row['firstname'], "\t|\t",
@@ -210,13 +208,44 @@ while( $row = mysql_fetch_assoc( $result)) {
 echo PHP_EOL;
 
 // Using perform_query6()
-$result = perform_query6( "fred", "fox");
+$result = perform_query6($link, "fred", "fox");
 echo "Result:", PHP_EOL;
 while( $row = mysql_fetch_assoc( $result)) {
   echo $row['firstname'], "\t|\t",
     $row['lastname'], "\t|\t",
     $row['age'], PHP_EOL;
 }
+
+/* same things, but not enclosed in functions... */
+$firstname = 'fred';
+$lastname = 'fox';
+// perform_query1
+$topquery = "SELECT firstname, lastname, age FROM friends 
+    WHERE firstname='$firstname' AND lastname='$lastname'";
+$result = mysql_query($topquery, $link);
+// perform_query2
+$topquery2 = "SELECT firstname, lastname, age FROM friends 
+    WHERE firstname='".$firstname."' AND lastname='".$lastname."'";
+$result = pg_query($link, $topquery2);
+// perform_query3
+$topquery3 = "SELECT firstname, lastname, age FROM friends WHERE firstname='";
+$topquery3 .= $firstname;
+$topquery3 .= "' AND lastname='";
+$topquery3 .= $lastname;
+$topquery3 .= "'";
+$result = sqlite_query($link, $topquery3);
+// perform_query4
+$topquery4 = "SELECT firstname, lastname, age FROM friends 
+    WHERE firstname='fred' AND lastname='fox'";
+$result = mysql_query($topquery4, $link);
+// perform_query5
+$result = sqlite_query("SELECT firstname, lastname, age FROM friends 
+    WHERE firstname='$firstname' AND lastname='$lastname'", $link);
+// perform_query6
+$result = sqlite_query($link, "SELECT firstname, lastname, age FROM friends 
+    WHERE firstname='".$firstname."' AND lastname='".$lastname."'");
+
+
 
 
 // free the resources associated with the result set
